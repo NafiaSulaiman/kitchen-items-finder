157578b702f70fbe3453b12c2bceaa75e54c78c7
Jashif <jashif.aboobacker@ingka.com>
2021-09-01 01:34:55 +0200

    added prettier

--- client/kitchen-finder/src/App.js
+++ client/kitchen-finder/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Shelf from './components/shelf';
+import logo from "./logo.svg";
+import "./App.css";
+import Shelf from "./components/shelf";
 
 function App() {
   return (
     <div className="App">
-     <header>My kitchen's</header>
-     <Shelf></Shelf>
+      <header>My kitchen's</header>
+      <Shelf></Shelf>
     </div>
   );
 }
--- client/kitchen-finder/src/components/shelf.js
+++ client/kitchen-finder/src/components/shelf.js
@@ -1,21 +1,26 @@
-import React from "react"
-function Shelf()
-{
-    const[shelfRespnse,setShelf]=React.useState()
-    React.useEffect(()=>{
-        fetch("https://catfact.ninja/fact")
-        .then((response)=>response.json())
-        .then(result=>{
-            console.log(result)
-            setShelf(result);
-        }).catch((error)=>{
-            console.log(error)
-        })
-    },[])
-    if(shelfRespnse)
-    return <div>{shelfRespnse.fact}{shelfRespnse.length}</div>
-    else{
-        return <div>{"no data"}</div>
-    }
+import React from "react";
+function Shelf() {
+  const [shelfRespnse, setShelf] = React.useState();
+  React.useEffect(() => {
+    fetch("https://catfact.ninja/fact")
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        setShelf(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+  if (shelfRespnse)
+    return (
+      <div>
+        {shelfRespnse.fact}
+        {shelfRespnse.length}
+      </div>
+    );
+  else {
+    return <div>{"no data"}</div>;
+  }
 }
 export default Shelf;
