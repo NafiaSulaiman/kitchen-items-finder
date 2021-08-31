3986b98c0b73b0d9b3fcfbc58dc9f543e0b6f62e
Jashif <jashif.aboobacker@ingka.com>
2021-09-01 01:22:23 +0200

    added frontned for kitchen

---
+++ client/kitchen-finder/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Shelf from './components/shelf';
+
+function App() {
+  return (
+    <div className="App">
+     <header>My kitchen</header>
+     <Shelf></Shelf>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/kitchen-finder/src/components/shelf.js
@@ -0,0 +1,21 @@
+import React from "react"
+function Shelf()
+{
+    const[shelfRespnse,setShelf]=React.useState()
+    React.useEffect(()=>{
+        fetch("https://catfact.ninja/fact")
+        .then((response)=>response.json())
+        .then(result=>{
+            console.log(result)
+            setShelf(result);
+        }).catch((error)=>{
+            console.log(error)
+        })
+    },[])
+    if(shelfRespnse)
+    return <div>{shelfRespnse.fact}{shelfRespnse.length}</div>
+    else{
+        return <div>{"no data"}</div>
+    }
+}
+export default Shelf;
